903ed460092596ba008ab54b19c4dc2a104d3f57
unknown <you@example.com>
2023-02-25 09:45:50 +0530

    added Habit Adder

--- habit_tracker/lib/main.dart
+++ habit_tracker/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:habit_tracker/screens/habit_adder.dart';
 import 'package:habit_tracker/screens/splashscreen.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Habit Tracker',
-      home: const SplashScreen(),
+      home: const Dashboard(),
     );
   }
 }
